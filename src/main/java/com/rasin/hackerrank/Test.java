18561c03afaf6e224813c147cc5d2aeca3dadd60
rasin <zibersaioros@gmail.com>
2018-09-11 11:18:11 +0900

    second commit

---
+++ src/main/java/com/rasin/hackerrank/Test.java
@@ -0,0 +1,7 @@
+package com.rasin.hackerrank;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/main/java/com/rasin/hackerrank/easy/ArraysLeftRotation.java
@@ -0,0 +1,30 @@
+package com.rasin.hackerrank.easy;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+
+public class ArraysLeftRotation {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int b[] = new int[n];
+
+        System.arraycopy(a, k, b, 0, n-k);
+        System.arraycopy(a, 0, b, n-k, k);
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
